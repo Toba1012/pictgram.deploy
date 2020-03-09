a9fd756538140e31a7c7f70a08f5b67014860f1b
Toba <marxdm02@gmail.com>
2020-03-09 15:22:30 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to:'favorites#destroy'
+  
+  get '/comments/:topic_id/new', to: 'comments#new', as: 'comments'
+  post '/comments/:topic_id/create', to: 'comments#create', as: 'comments_create'
+  
+end
